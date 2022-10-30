5d8776e3b58137d198a180cd10757285b2c76444
slasken06 <56585404+asciiboi@users.noreply.github.com>
2022-10-30 22:23:37 +0100

    Fixed it and added randomness

--- README.md
+++ README.md
@@ -9,5 +9,5 @@ pip install -r requirements.txt
 
 # When you want to start it
 
-Fill out the strings in the file labled Username and password with your username and password
+Fill out the strings in the main.py file labled Username and password with your username and password
 and then run the main.py file to start
--- main.py
+++ main.py
@@ -1,21 +1,39 @@
-from selenium import webdriver 
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-from time import sleep
-
-url = "https://at4.typewriter.at"
-
 # Login details
 username = ""
 password = ""
 
 # Config options
-waitTimeBetweenLetters = 0.03
+waitTimeBetweenLetters = 0.001
+do_random_shit = True
+min_random_add_wait = 0.05
+max_random_add_wait = 0.15
 loadWait = 5
+mistake_percentage = 0.005
+
+
+
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+from selenium.webdriver import Keys, ActionChains
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from random import randrange, random
+from time import sleep
+
+url = "https://at4.typewriter.at"
+
+
+def get_random_pause():
+    if do_random_shit:
+        return randrange(int(min_random_add_wait*1000), int(max_random_add_wait*1000))/1000
+    return 0
+
+def is_mistake():
+    return random() < mistake_percentage
 
 # Open website
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
 driver.get(url)
 sleep(loadWait)
 
@@ -24,22 +42,39 @@ driver.find_element(By.ID, "LoginForm_username").send_keys(username)
 driver.find_element(By.ID, "LoginForm_pw").send_keys(password)
 driver.find_element(By.XPATH, "/html/body/div[3]/div[2]/div[1]/form/div[3]/input").click()
 sleep(loadWait)
+# Start thing
 driver.find_element(By.XPATH, "/html/body/div[3]/div[3]/div[1]/div[1]/a").click()
 sleep(loadWait)
 driver.find_element(By.XPATH, "/html/body/div[6]/div[3]/div/button").click()
-
+sleep(0.25)
 # Do letter loop
 while True:
-    # Find the current letter
-    currentLetter = driver.find_element(By.XPATH, "/html/body/div[3]/div[2]/div[4]/div[2]/div[2]/span[1]").get_attribute('innerHTML')
-    currentLetter = str(currentLetter)
-
-    # If is nobreakspace set space
-    if currentLetter == "&nbsp;":
-        currentLetter = " "
-
-    if not currentLetter.lower() in ["ö","ä","ü"]:
-        print(currentLetter)
-        driver.find_element(By.XPATH, "/html/body").send_keys(currentLetter)
-    else:
-        print("Manual input required!!!")
+    try:
+        # Find the current letter
+        currentLetter = driver.find_element(By.ID, "actualLetter").get_attribute('innerHTML')
+        try:
+            remainingText = driver.find_element(By.ID, "remainingText").get_attribute('innerHTML')
+        except:
+            remainingText = ""
+        
+    except:
+        print("No more text found")
+        break
+    if remainingText == "":
+        break
+    to_type = currentLetter+remainingText
+    to_type = to_type.replace("&nbsp;", " ")
+    to_type = to_type.replace(" ", Keys.SPACE)
+    
+    actions = ActionChains(driver)
+    for i in range(len(to_type)):
+        if is_mistake():
+            if i == 0 or len(to_type) < 2: idx = 1 
+            else: idx = 0
+            actions.send_keys(to_type[0])\
+                .pause(waitTimeBetweenLetters+get_random_pause())
+        
+        actions.send_keys(to_type[i])\
+            .pause(waitTimeBetweenLetters+get_random_pause())
+    
+    actions.perform()
